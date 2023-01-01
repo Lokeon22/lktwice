37a43cf146a8e71af1d6257f692c8f51e0a7c2c5
Gabriel LK <gabrielfelipe941@yahoo.com.br>
2023-01-01 15:09:13 -0300

    footer done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,51 +1,91 @@
-import { Container, SubContainer, Content, Links, Botoes } from "./style";
+import {
+  Container,
+  SubContainer,
+  Content,
+  Links,
+  Botoes,
+  Foot,
+  LinksMobile,
+} from "./style";
+
+import facebook from "../../assets/icons/facebook.svg";
+import youtube from "../../assets/icons/youtube.svg";
+import instagram from "../../assets/icons/instagram.svg";
 
 export const Footer = () => {
   return (
-    <Container>
-      <SubContainer>
-        <Content>
-          <h2>Content</h2>
-          <ul>
-            <li>
-              <a href="#">Profile</a>
-            </li>
-            <li>
-              <a href="#">News</a>
-            </li>
-            <li>
-              <a href="#">Discography</a>
-            </li>
-            <li>
-              <a href="#">Special</a>
-            </li>
-            <li>
-              <a href="#">FanClub</a>
-            </li>
-          </ul>
-        </Content>
-        <Links>
-          <h2>Links</h2>
-          <ul>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">Facebook</a>
-            </li>
-            <li>
-              <a href="#">Instagram</a>
-            </li>
-            <li>
-              <a href="#">YouTube</a>
-            </li>
-          </ul>
-        </Links>
-        <Botoes>
-          <button>Twice Official FanClub Once</button>
-          <button>Twice Official FanClub Once</button>
-        </Botoes>
-      </SubContainer>
-    </Container>
+    <>
+      <Container>
+        <SubContainer>
+          <Content>
+            <h2>Content</h2>
+            <ul>
+              <li>
+                <a href="#">Profile</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+              <li>
+                <a href="#">Discography</a>
+              </li>
+              <li>
+                <a href="#">Special</a>
+              </li>
+              <li>
+                <a href="#">FanClub</a>
+              </li>
+            </ul>
+          </Content>
+          <Links>
+            <h2>Link</h2>
+            <ul>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Instagram</a>
+              </li>
+              <li>
+                <a href="#">YouTube</a>
+              </li>
+            </ul>
+          </Links>
+          <LinksMobile>
+            <h2>Link</h2>
+            <ul>
+              <a href="#">
+                <img src={instagram} />
+              </a>
+              <a href="#">
+                <img src={youtube} />
+              </a>
+              <a href="#">
+                <img src={facebook} />
+              </a>
+              <a href="#">
+                <img src={youtube} />
+              </a>
+              <a href="#">
+                <img src={instagram} />
+              </a>
+            </ul>
+          </LinksMobile>
+          <Botoes>
+            <button>Twice Official FanClub Once</button>
+            <button>Twice Official FanClub Once</button>
+          </Botoes>
+        </SubContainer>
+      </Container>
+      <Foot>
+        <span>
+          Copyright <strong>© LK ENTERTAINMENT</strong>, Ltd. All rights
+          reserved.
+        </span>
+      </Foot>
+    </>
   );
 };
--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -2,16 +2,21 @@ import styled from "styled-components";
 
 export const Container = styled.section`
   width: 100%;
-  height: 280px;
+  height: 300px;
   background-color: rgb(45, 45, 45);
 `;
 
 export const SubContainer = styled.div`
-  max-width: 1100px;
+  max-width: 900px;
   display: grid;
   grid-template-columns: 1fr 1fr 2fr;
   margin: 0 auto;
-  padding-top: 40px;
+  padding-top: 70px;
+
+  @media screen and (max-width: 769px) {
+    grid-template-columns: 1fr;
+    padding-top: 10px;
+  }
 `;
 
 export const Content = styled.div`
@@ -19,18 +24,93 @@ export const Content = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  gap: 5px;
+
+  h2 {
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    border-bottom: 1px solid white;
+    padding: 4px 0px;
+  }
 
   ul {
     list-style: none;
+    text-transform: uppercase;
+
+    li {
+      margin-top: 7px;
+      font-size: 0.8rem;
+
+      a {
+        color: rgb(204, 204, 204);
+        :hover {
+          color: #cccccccc;
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: 769px) {
+    display: none;
   }
 `;
 
 export const Links = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 5px;
+
+  h2 {
+    font-size: 1.125rem;
+    text-transform: uppercase;
+    padding: 4px 0px;
+  }
 
   ul {
     list-style: none;
+
+    li {
+      margin-top: 7px;
+      font-size: 0.9rem;
+      a {
+        color: rgb(204, 204, 204);
+        :hover {
+          color: #cccccccc;
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 769px) {
+    display: none;
+  }
+`;
+
+export const LinksMobile = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+
+  h2 {
+    font-size: 0.9rem;
+    text-transform: uppercase;
+    text-align: center;
+    margin: 20px;
+  }
+
+  ul {
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    justify-content: center;
+
+    img {
+      width: 20px;
+      margin-bottom: 20px;
+    }
+  }
+
+  @media screen and (min-width: 769px) {
+    display: none;
   }
 `;
 
@@ -38,15 +118,45 @@ export const Botoes = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  gap: 20px;
+  gap: 30px;
+  padding: 0px 10px;
 
   button {
-    width: 400px;
+    width: 450px;
     padding: 16px;
     background-color: rgb(68, 68, 68);
     color: rgb(153, 153, 153);
     border: 0;
     cursor: pointer;
     text-transform: uppercase;
+  }
+
+  @media screen and (max-width: 769px) {
+    align-items: center;
+    padding: 0 30px;
+
+    button {
+      width: 100%;
+    }
+  }
+`;
+
+export const Foot = styled.footer`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  margin-top: 10px;
+
+  span {
+    font-size: 0.9rem;
+  }
+
+  strong {
+    font-weight: 400;
+    font-size: 0.7rem;
+    color: red;
   }
 `;
